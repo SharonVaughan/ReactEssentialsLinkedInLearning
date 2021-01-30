7fd64cb0a889a588b750bac3057e5ec51c9c8692
Sharon Vaughan <sharon.vaughan@envato.com>
2021-01-30 12:45:28 +1100

    Conditional rendering
    Two methods used. One using if..else. The other a
    JSX inline ternary expression.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,24 @@
 import './App.css';
-import restaurant from './restaurant.jpg';
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  )
+function SecretComponent() {
+  return <h1>Secret information for authorised users only</h1>
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      <img src={restaurant} height={200} alt="restaurant" /><br />
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) =>
-          <li key={dish.id}>{dish.title}</li>)}
-      </ul>
-    </section>
-  )
+function RegularComponent() {
+  return <h1>Everyone can see this component</h1>
 }
 
-function Footer(props) {
+function App(props) {
   return (
-    <footer>
-      <p>
-        Copyright {props.year}
-      </p>
-    </footer>
+    <>
+      {props.authorised ? <SecretComponent /> : <RegularComponent />}
+    </>
   )
-}
-
-const dishes = [
-  "Macaroni and Cheese",
-  "Salmon",
-  "Tofu with Vegetables",
-  "Minestrone"
-]
-
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }))
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Cindy" />
-      <Main adjective="amazing" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+  // if(props.authorised) {
+  //   return <SecretComponent />
+  // } else {
+  //   return <RegularComponent />
+  // }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-function AppTwo() {
-  return <h1>This is the Second App</h1>
-}
-
 ReactDOM.render(
-  <>
-    <App />
-    <AppTwo />
-  </>,
+  <App authorised={true} />,
   document.getElementById('root')
 );
