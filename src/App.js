084e0bd5a540515f79620464b3739ca01d09e09b
Sharon Vaughan <sharon.vaughan@envato.com>
2021-02-06 15:33:20 +1100

    Introducing React testing library
    Automatically included when we use create-react-app
    Tests if rendering elements is correct
    In test file, import "@testing-library/react" and "React"
    In this case, use `getByText` to destructure `render`,
    which is called a query.
    With React Testing Library, a query is going to reach out
    to some sort of element and return some information about it.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import React, { useState, useEffect } from "react"
 import './App.css';
 
-// https://api.github.com/users/sharonvaughan
+function App() {
 
-function App({ login }) {
-  const [data, setData] = useState(null)
-  const[loading, setLoading] = useState(false)
-  const[error, setError] = useState(null)
-
-  useEffect(() => {
-    if(!login) return
-    setLoading(true)
-    fetch(`https://api.github.com/users/${login}`)
-    .then(response => response.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError)
-  }, [login])
-
-  if(loading) return <h1>Loading...</h1>
-  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
-  if(!data) return null
 
   return (
     <div>
-      <h1>{ data.name }</h1>
-      <h4>{data.bio}</h4>
-      <img src={data.avatar_url} alt={data.login}/>
+      <h1>Hello React Testing Library</h1>
     </div>
   )
 }
