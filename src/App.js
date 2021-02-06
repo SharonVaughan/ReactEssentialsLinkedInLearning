d8ccb4996b788d266aa9efc06841504eb8ef487b
Sharon Vaughan <sharon.vaughan@envato.com>
2021-02-06 14:22:01 +1100

    Handling loading states
    Three possible States: pending/loading, success and failed/error.
    Some adjustments to our component to make this work a little nicer,
    with useState.

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import './App.css';
 
 function App({ login }) {
   const [data, setData] = useState(null)
+  const[loading, setLoading] = useState(false)
+  const[error, setError] = useState(null)
 
   useEffect(() => {
+    if(!login) return
+    setLoading(true)
     fetch(`https://api.github.com/users/${login}`)
     .then(response => response.json())
     .then(setData)
-  }, [])
+    .then(() => setLoading(false))
+    .catch(setError)
+  }, [login])
 
-  if(data) {
-    return (<div>
+  if(loading) return <h1>Loading...</h1>
+  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if(!data) return null
+
+  return (
+    <div>
       <h1>{ data.name }</h1>
       <h4>{data.bio}</h4>
       <img src={data.avatar_url} alt={data.login}/>
-    </div>)
-  }
-
-  return <div>No User Available</div>
+    </div>
+  )
 }
 
 export default App;
